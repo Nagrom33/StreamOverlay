e445f0349bcaeb803b08bc4722fa55c923aaf883
Gurdt <gerroy@gerroy.nl>
2023-12-12 21:33:10 +0100

    Started route Controls

---
+++ src/app/(routes)/controls/page.tsx
@@ -0,0 +1,10 @@
+export default function Controls() {
+
+  return (
+    <main
+      className="relative flex items-center justify-center w-[1920px] h-[1080px]"
+    >
+        <p>Buttons komen hier</p>
+    </main>
+  )
+}
