5b4c3c7cc44dd3fd20e08d4eb030dcc5aee31b7d
Gurdt <gerroy@gerroy.nl>
2023-12-12 14:08:29 -0800

    Added env

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        NEXT_SOCKET_SERVER: process.env.NEXT_SOCKET_SERVER,
+    }
+}
 
 module.exports = nextConfig
--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,5 +1,5 @@
 import { io, Socket } from 'socket.io-client';
 
-const socket: Socket = io('https://socket.gerroy.nl');
+const socket: Socket = io(process.env.NEXT_SOCKET_SERVER as string);
 
 export default socket;
