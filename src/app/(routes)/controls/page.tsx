d6ee95d30bcc68e409c8fb4aab72dafc9664cc6c
Gurdt <gerroy@gerroy.nl>
2023-12-12 12:42:06 -0800

    Started with controls

--- src/app/(routes)/controls/page.tsx
+++ src/app/(routes)/controls/page.tsx
@@ -2,9 +2,16 @@ export default function Controls() {
 
   return (
     <main
-      className="relative flex items-center justify-center w-[1920px] h-[1080px]"
+      className="grid grid-cols-2 gap-4"
     >
-        <p>Buttons komen hier</p>
+        <button className="p-4 bg-blue-500 text-white">Button 1</button>
+        <button className="p-4 bg-green-500 text-white">Button 2</button>
+        <button className="p-4 bg-red-500 text-white">Button 3</button>
+        <button className="p-4 bg-yellow-500 text-white">Button 4</button>
+        <button className="p-4 bg-purple-500 text-white">Button 5</button>
+        <button className="p-4 bg-indigo-500 text-white">Button 6</button>
+        <button className="p-4 bg-pink-500 text-white">Button 7</button>
+        <button className="p-4 bg-gray-500 text-white">Button 8</button>
     </main>
   )
 }
