33eb74378392766ada64f12962b8e9734df5c1c0
Gurdt <gerroy@gerroy.nl>
2023-12-12 15:00:20 -0800

    Added spin delay on repeat

--- src/components/N033Logo/index.tsx
+++ src/components/N033Logo/index.tsx
@@ -2,22 +2,30 @@ import Image from 'next/image'
 import { motion } from 'framer-motion'
 
 export default function N033Logo() {
-  
-    return (
-      <motion.div
-        className="logo absolute right-9 bottom-9"
-        initial={{ rotateY: 0 }}
-        animate={{ rotateY: 360 }}
-        transition={{ delay: 10, repeat: Infinity, duration: 2 }}
-      >
-        <Image
-          src="./assets/images/N033.svg"
-          width={200}
-          height={100}
-          alt="N033 Logo"
-          className="drop-shadow"
-        />
-      </motion.div>
-    )
-  }
-  
+  const rotateVariants = {
+    initial: { rotateY: 0 },
+    animate: { rotateY: 360 },
+  };
+  return (
+    <motion.div
+      className="logo absolute right-9 bottom-9"
+      key="logo-animation"
+      variants={rotateVariants}
+      initial="initial"
+      animate="animate"
+      transition={{
+        duration: 2,
+        repeat: Infinity,
+        repeatDelay: 30
+      }}
+    >
+      <Image
+        src="./assets/images/N033.svg"
+        width={200}
+        height={100}
+        alt="N033 Logo"
+        className="drop-shadow"
+      />
+    </motion.div>
+  )
+}
